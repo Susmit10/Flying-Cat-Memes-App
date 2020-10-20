82aa8c9c9723516486985e425f76371e62044eef
SUSMIT RUDRA <60251577+Susmit10@users.noreply.github.com>
2020-10-20 11:00:11 +0530

    Basic UI

--- app/src/main/java/com/example/flyingcatmemes/MainActivity.kt
+++ app/src/main/java/com/example/flyingcatmemes/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.flyingcatmemes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun nextMeme(view: View) {
+        Toast.makeText(this, "Next", Toast.LENGTH_SHORT).show()
+    }
+    fun shareMeme(view: View) {
+        Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show()
     }
 }
